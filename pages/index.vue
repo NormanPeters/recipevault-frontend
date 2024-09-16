d88b057e6569c6c44eaa4999aa434f92d2068d6c
Barrique-v1 <peters.norman91@gmail.com>
2024-09-16 09:05:01 +0200

    feat: add folder structure and tailwind

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <p class="font-bold">Click on the links above to navigate to different pages</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <h1>Login Page</h1>
+  <p>Click on the links above to navigate to different pages</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
