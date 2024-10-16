f945f6937ff15ef4c5ad8fc20df83d7445c20d25
Barrique-v1 <peters.norman91@gmail.com>
2024-10-16 18:46:23 +0200

    feat: add password show / hide btn

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -8,8 +8,18 @@
         </div>
         <h1 class="font-syncopate text-3xl font-bold text-center my-10">Recipe Vault</h1>
         <form @submit.prevent="handleLogin">
+          <span class="font-bold">Username</span>
           <TextInput id="username" label="Username" v-model="username" class="mb-4"/>
-          <TextInput id="password" type="password" label="Password" v-model="password" class="mb-4"/>
+          <span class="font-bold">Password</span>
+          <div class="relative mb-4">
+            <TextInput :type="showPassword ? 'text' : 'password'" id="password" label="Password" v-model="password"
+                       class="w-full"/>
+            <button type="button" @click="togglePasswordVisibility"
+                    class="absolute inset-y-0 right-0 pr-3 flex items-center leading-5">
+              <EyeIcon v-if="!showPassword" class="h-5 w-5"/>
+              <EyeSlashIcon v-else class="h-5 w-5"/>
+            </button>
+          </div>
           <div class="flex items-center justify-between">
             <PrimaryButton label="Login" type="submit"/>
             <a class="inline-block align-baseline font-bold text-sm text-primary hover:text-primary-hover" href="">
@@ -23,19 +33,21 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { BookOpenIcon } from "@heroicons/vue/24/solid";
-import { api } from "~/services/api";
+import {ref} from 'vue';
+import {useRouter} from 'vue-router';
+import {BookOpenIcon} from "@heroicons/vue/24/solid";
+import {api} from "~/services/api";
+import {EyeIcon, EyeSlashIcon} from "@heroicons/vue/24/outline";
 
 // Reactive state for username and password fields
 const username = ref<string>('');
 const password = ref<string>('');
+const showPassword = ref<boolean>(false);
 const router = useRouter();
 
 const handleLogin = async () => {
   try {
-    const token = await api.login({ username: username.value, password: password.value });
+    const token = await api.login({username: username.value, password: password.value});
     if (token) {
       localStorage.setItem("authToken", token);
       console.log("Login successful");
@@ -46,6 +58,10 @@ const handleLogin = async () => {
   } catch (error) {
     console.error("Error during login:", error);
   }
+};
+
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
 };
 </script>
 
