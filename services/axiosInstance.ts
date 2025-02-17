8894f0e21cf2bd60f9ecb0c6b4c3f7f475f89e2f
Barrique-v1 <peters.norman91@gmail.com>
2025-02-17 18:30:57 +0100

    update backend url

--- services/axiosInstance.ts
+++ services/axiosInstance.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL:
         // 'http://localhost:8080/api',
-        'https://recipevault-backend.onrender.com/api',
+        'https://barriquebackend.onrender.com/api',
     withCredentials: true,
 });
 
